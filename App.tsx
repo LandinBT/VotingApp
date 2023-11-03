88f224884c7901d199aa2a3ce3f6341b9e6982b3
LandinBT <daniel.basulto0859@alumnos.udg.mx>
2023-11-03 00:16:40 -0600

    Simplifying Code

--- App.tsx
+++ App.tsx
@@ -8,22 +8,14 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { StatusBar, Text, useColorScheme } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 
 import NAVIGATION from './Navigation';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
     <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
       <NAVIGATION />
     </NavigationContainer>
   );
