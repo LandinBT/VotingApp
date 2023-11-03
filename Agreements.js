3b23d018fddf88a1a047ea3015e59ba5b4fa8658
LandinBT <daniel.basulto0859@alumnos.udg.mx>
2023-11-03 00:16:59 -0600

    Add icons

--- Agreements.js
+++ Agreements.js
@@ -1,12 +1,46 @@
+//"https://votingapp2023.000webhostapp.com/data.php?"
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
+
+/**
+const renderItem = ({ item }) => (
+  <View>
+    <Text>{item.title}</Text>
+    <Button title="Votar" onPress={() => navigation.navigate('Voting', { agreement: item })} />
+    <Button title="Resultados" onPress={() => navigation.navigate('Results', { agreement: item })} />
+  </View>
+);
+*/
+
+/**
+constructor(props) {
+ super(props);
+    this.state = {
+      modalVisibleEmail: false,
+
+      description: "",
+      votes_in_favor: 0,
+      votes_against: 0,
+      abstencions: 0
+    };
+  }
+ */
 
 export default class Agreements extends Component {
+
   render() {
     return (
+      <Text>Hola Mundo!</Text>
+      /*
       <View>
-        <Text> Acuerdos </Text>
+        <FlatList
+          data={agreements}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
       </View>
+    */
     )
   }
+
 }
--- Navigation.js
+++ Navigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Agreements from './Agreements';
 import Voting from './Voting';
@@ -11,9 +12,42 @@ export default class Navigation extends Component {
     render() {
         return (
             <Tab.Navigator>
-                <Tab.Screen name="Agreements" component={Agreements} options={{ headerShown: false }} />
-                <Tab.Screen name="Voting" component={Voting} options={{ headerShown: false }} />
-                <Tab.Screen name="Results" component={VotingResults} options={{ headerShown: false }} />
+                <Tab.Screen
+                    name="Agreements"
+                    component={Agreements}
+                    options={{
+                        headerShown: false,
+                        tabBarLabel: 'Agreements',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="file-document-multiple" color={color} size={size} />
+                        ),
+                    }}
+                />
+
+                <Tab.Screen
+                    name="Voting"
+                    component={Voting}
+                    options={{
+                        headerShown: false,
+                        tabBarLabel: 'Voting',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="vote" color={color} size={size} />
+                        ),
+                    }}
+                />
+
+                <Tab.Screen
+                    name="Results"
+                    component={VotingResults}
+                    options={{
+                        headerShown: false,
+                        tabBarLabel: 'Results',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="list-status" color={color} size={size} />
+                        ),
+                    }}
+                />
+
             </Tab.Navigator>
         )
     }
